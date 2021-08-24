544d8ed4f3d2e31b6f9bd0d49a303948b814c5c0
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-25 04:58:21 +0530

    ADDED: To initialize gambling stakes for every day and every game

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.gamblingsimulation;
 
 public class GamblingSimulator {
+	
+	public static final int STAKES_EVERYDAY = 100;
+	public static final int STAKES_EVERYGAME = 1;
 
 	public static void main(String[] args) {
-
+		
 	}
-
 }
