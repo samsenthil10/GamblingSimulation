878a0ec091d843cfc5ed89237b6c52c7e585c6b2
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-25 05:01:24 +0530

    ADDED: To win or lose bet for one game

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
@@ -1,11 +1,26 @@
 package com.bridgelabz.gamblingsimulation;
 
 public class GamblingSimulator {
-	
+
 	public static final int STAKES_EVERYDAY = 100;
 	public static final int STAKES_EVERYGAME = 1;
+	public static final int WIN = 1;
+	public static final int LOSE = 0;
+
+	public static int everyGameBetOutcome(int cash) {
+		int gameState = (int)Math.floor(Math.random()*10)%2;
+		if(gameState == WIN) {
+			cash++;
+		}
+		else if(gameState == LOSE) {
+			cash--;
+		}
+		return cash;
+	}
 
 	public static void main(String[] args) {
-		
+		int netCash = STAKES_EVERYDAY;
+		netCash = everyGameBetOutcome(netCash);
+		System.out.println(netCash);
 	}
 }
