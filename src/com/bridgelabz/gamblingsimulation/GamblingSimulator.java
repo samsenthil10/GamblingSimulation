024d669ee2a51a22752a14500be12085ea0e44d3
Senthil Kumar S A <sasenthilk@gmail.com>
2021-08-25 05:12:38 +0530

    ADDED: To calculate number of days bets won or lost in a month

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
@@ -7,6 +7,9 @@ public class GamblingSimulator {
 	public static final int WIN = 1;
 	public static final int LOSE = 0;
 	public static final int TOTAL_DAYS = 20;
+	
+	public static int countLose=0;
+	public static int countWin=0;
 
 	public static int everyGameBetOutcome(int cash) {
 		int gameState = (int)Math.floor(Math.random()*10)%2;
@@ -26,6 +29,12 @@ public class GamblingSimulator {
 		{
 			netCash = everyGameBetOutcome(netCash);
 		}
+		if(netCash == max) {
+			countWin++;
+		}
+		else if(netCash == min) {
+			countLose++;
+		}
 		return netCash;
 	}
 
@@ -37,10 +46,25 @@ public class GamblingSimulator {
 		}
 		return netCash;
 	}
-
+	
+	public static void loseOrWinCounter() {
+		System.out.println("Days Won: "+countWin);
+		System.out.println("Days Lost: "+countLose);
+		if(countWin > countLose){
+			System.out.println("Won by "+(countWin-countLose)+" Days");
+		}
+		else if(countLose > countWin){
+			System.out.println("Lost by "+(countLose-countWin)+" Days");
+		}
+		else {
+			System.out.println("Draw Game");
+		}
+	}
+	
 	public static void main(String[] args) {
 		int netCash = STAKES_EVERYDAY;
 		netCash = everyMonthBetOutcome(netCash);
 		System.out.println(netCash);
-	}	
+		loseOrWinCounter();
+	}
 }
